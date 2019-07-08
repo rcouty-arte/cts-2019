af13ebdc1ce5824eb87e64f11458f9897e221a5c
MaximeSempere <maximesempere@gmail.com>
2019-07-08 17:08:18 +0200

    Updates

---
+++ precache-manifest.1c44631d82a27e73e5f6e5ae7becf397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ecc3c03c70852ee381c006e23972083",
+    "url": "/cts-2019/index.html"
+  },
+  {
+    "revision": "4d0739d9a2220823950b",
+    "url": "/cts-2019/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cts-2019/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4d0739d9a2220823950b",
+    "url": "/cts-2019/static/js/main.6aa4cd20.chunk.js"
+  },
+  {
+    "revision": "3b6218594efb2ecfb35d",
+    "url": "/cts-2019/static/js/runtime~main.0c495646.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cts-2019/static/media/logo.5d5d9eef.svg"
+  }
+]);
