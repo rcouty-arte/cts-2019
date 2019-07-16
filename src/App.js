808aef0b61a1b4dafd10a42f5781579064b2193e
MaximeSempere <maximesempere@gmail.com>
2019-07-16 15:11:29 +0200

    adding special route for gh-pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <Router>
       <Route exact path="/" component={StopSearch} />
+      <Route exact path="/cts-2019/" component={StopSearch} />
     </Router>
   );
 }
